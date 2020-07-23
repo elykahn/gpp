a37f6455bf5295a2e53feb43b0cca49ffb6acfd5
Ely Kahn <ely.kahn@gmail.com>
2020-07-23 18:53:11 -0500

    Update prismic-config.js

--- prismic-config.js
+++ prismic-config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // -- Prismic repository GraphQL endpoint
-  apiEndpoint: 'https://your-repo-name.prismic.io/graphql',
+  apiEndpoint: 'https://prismic.ely.io.prismic.io/graphql',
 };
